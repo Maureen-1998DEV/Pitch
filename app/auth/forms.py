f4c16d967e52c98ab161c8db4c1b122fae8a5d02
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-20 14:02:13 +0300

    auth in templates

---
+++ app/auth/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    firstname = StringField('Enter your first name',validators = [Required()])
+    lastname = StringField('Enter your last name',validators = [Required()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+    
+class RegistrationForm(FlaskForm):
+    # .......
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('Account already exists with that Emaail')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('username  already taken')
